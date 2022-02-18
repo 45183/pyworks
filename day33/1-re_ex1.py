b45c74bc9ca75e6105b57971c46fca2b909187c3
45183 <hsb9743@naver.com>
2022-02-18 12:49:45 +0900

    day33

---
+++ day33/1-re_ex1.py
@@ -0,0 +1,23 @@
+import re
+
+pattern1 = re.compile("[a-z]")             # 정규 표현식
+pattern2 = re.compile("[a-z]+")            # 처음 문자부터 검색
+result1 = pattern1.match("afternoon")
+result2 = pattern2.match("afternoon")
+print(result1)
+print(result2)
+
+
+pattern3 = re.compile("[a-z]+")
+pattern4 = re.compile("[0-9]+\s[a-z]+")
+result3 = pattern3.match("2022 python")
+result4 = pattern4.match("2022 python")
+print(result3)
+print(result4)
+
+
+# [a-z] : a~z 내에서 1개
+# [a-z]+ : a~z 내에서 연결되는거
+# \s : 빈 공백
+# \d : [0-9]
+# \w : [a-zA-Z0-9]
