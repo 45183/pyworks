532e967854b90a7fd240277617bee6c55222227b
45183 <hsb9743@naver.com>
2022-03-16 09:07:59 +0900

    day49

--- member/ranking1.py
+++ member/ranking1.py
@@ -23,3 +23,31 @@ for i in a:
         print(str(idx) + "번 학생은 2급입니다.")
     else:
         print(str(idx) + "번 학생은 불합격입니다.")
+
+
+print()
+
+a_idx = 0
+
+for i in a:
+    a_idx += 1
+    if i >= 70:
+        result = "1급"
+    elif i >=60:
+        result = "2급"
+    else:
+        result = "불합격"
+    print(str(a_idx) + "번 학생은" + result + "입니다.")
+
+print()
+
+n = len(a)
+
+for i in range(0, n):
+    if a[i] >= 70:
+        result = "1급"
+    elif a[i] >= 60:
+        result = "2급"
+    else:
+        result = "불합격"
+    print("{}번 학생은 {}입니다.".format(i+1, result))
